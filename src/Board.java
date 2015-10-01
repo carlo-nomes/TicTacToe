87a0c0e12471b04cf19dcff26861393d0e0cec70
Carlo Nomes <nomes.carlo@gmail.com>
2015-10-01 11:51:22 +0200

    working update Carlo

--- src/Board.java
+++ src/Board.java
@@ -11,127 +11,71 @@ public class Board {
         board = new char[3][3];
         for (int y = 0; y < board.length; y++) {
             for (int x = 0; x < board[y].length; x++) {
-                board[y][x] = Character.forDigit((y *board.length) + x, 10);
+                board[y][x] = Character.forDigit((y * board.length) + x, 10);
             }
         }
-        currentPlayer = new Player("x");
     }
 
     public void play() {
-        int keuze;
-        System.out.println("Speler 'X' zal eerst gaan daarna mag currentPlayer 'O'");
+        currentPlayer = new Player('X');
 
-        try {
-            do {
-                currentBoard();
-                System.out.println("Speler '" + getPlayer() + "' kies een positie");
-                boolean positieGenomen = true;
-                while (positieGenomen) {
-                    Scanner sc = new Scanner(System.in);
-                    keuze = sc.nextInt();
-                    positieGenomen = checkPositie(keuze);
-                    if (positieGenomen == false) {
-                        positie[keuze] = getPlayer().charAt(0);
-                    }
-                }
-                nextPlayer();
-            } while (checkWinner().equalsIgnoreCase(""));
-        } catch (ArrayIndexOutOfBoundsException aiob) {
-            System.err.println("De keuze past niet in dit bord.");
+        Scanner scanner = new Scanner(System.in);
+        while (checkWin() == '0') {
+            System.out.println("geef positie om te plaatsen. speler " + currentPlayer.getNaam());
+            if (!setPlayer(scanner.nextInt()))
+                System.out.println("mag ni");
+            System.out.println(toString());
         }
-
+        System.out.printf("proficiat " + checkWin());
     }
 
-    public String checkWinner() {
-        String winnaar = "";
-        if (positie[1] == 'x' && positie[2] == 'x' && positie[3] == 'x') {
-            winnaar = "Speler x wint";
-            currentBoard();
+    private char checkWin() {
+        for (int y = 0; y < board.length; y++) {
+            if (board[y][0] == board[y][1] && board[y][1] == board[y][2])
+                return board[y][0];
         }
-        if (positie[4] == 'x' && positie[5] == 'x' && positie[6] == 'x') {
-            winnaar = "Speler x wint";
-            currentBoard();
+        for (int x = 0; x < board[0].length; x++) {
+            if (board[0][x] == board[1][x] && board[1][x] == board[2][x])
+                return board[0][x];
         }
-        if (positie[7] == 'x' && positie[8] == 'x' && positie[9] == 'x') {
-            winnaar = "Speler x wint";
-            currentBoard();
-        }
-        if (positie[1] == 'x' && positie[4] == 'x' && positie[7] == 'x') {
-            winnaar = "Speler x wint";
-            currentBoard();
-        }
-        if (positie[2] == 'x' && positie[5] == 'x' && positie[8] == 'x') {
-            winnaar = "Speler x wint";
-            currentBoard();
-        }
-        if (positie[3] == 'x' && positie[6] == 'x' && positie[9] == 'x') {
-            winnaar = "Speler x wint";
-            currentBoard();
-        }
-        if (positie[3] == 'x' && positie[5] == 'x' && positie[7] == 'x') {
-            winnaar = "Speler x wint";
-            currentBoard();
-        }
-        if (positie[1] == 'x' && positie[5] == 'x' && positie[9] == 'x') {
-            winnaar = "Speler x wint";
-            currentBoard();
-        }
-
-        if (positie[1] == 'o' && positie[2] == 'o' && positie[3] == 'o') {
-            winnaar = "Speler o wint";
-            currentBoard();
-        }
-        if (positie[4] == 'o' && positie[5] == 'o' && positie[6] == 'o') {
-            winnaar = "Speler o wint";
-            currentBoard();
-        }
-        if (positie[7] == 'o' && positie[8] == 'o' && positie[9] == 'o') {
-            winnaar = "Speler o wint";
-            currentBoard();
-        }
-        if (positie[1] == 'o' && positie[4] == 'o' && positie[7] == 'o') {
-            winnaar = "Speler o wint";
-            currentBoard();
-        }
-        if (positie[2] == 'o' && positie[5] == 'o' && positie[8] == 'o') {
-            winnaar = "Speler o wint";
-            currentBoard();
-        }
-        if (positie[3] == 'o' && positie[6] == 'o' && positie[9] == 'o') {
-            winnaar = "Speler o wint";
-            currentBoard();
-        }
-        if (positie[3] == 'o' && positie[5] == 'o' && positie[7] == 'o') {
-            winnaar = "Speler o wint";
-            currentBoard();
-        }
-        if (positie[1] == 'o' && positie[5] == 'o' && positie[9] == 'o') {
-            winnaar = "Speler o wint";
-            currentBoard();
-        }
-        if (!winnaar.equalsIgnoreCase("")) {
-            System.out.println(winnaar);
-        }
-        return winnaar;
+        if (board[0][0] == board[1][1] && board[1][1] == board[2][2])
+            return board[0][0];
+        if (board[0][2] == board[1][1] && board[1][1] == board[2][0])
+            return board[0][2];
+        else
+            return '0';
     }
 
-    public boolean checkPositie(int spot) {
-        if (positie[spot] == 'x' || positie[spot] == 'o' || spot > 9) {
-            System.out.println("Deze positie is al gebruikt, gelieve een andere te kiezen.");
-            return true;
-        } else {
+    public boolean isFree(int pos) {
+        int y = pos / board.length;
+        int x = pos % board.length;
+
+        if (board[y][x] == 'X' || board[y][x] == 'O')
             return false;
-        }
+        else
+            return true;
     }
 
-    public String getPlayer() {
+    public boolean setPlayer(int pos) {
+        if (!isFree(pos)) return false;
+
+        int y = pos / board.length;
+        int x = pos % board.length;
+
+        board[y][x] = currentPlayer.getNaam();
+
+        nextPlayer();
+        return true;
+    }
+
+    public char getPlayer() {
         return currentPlayer.getNaam();
     }
 
     public void nextPlayer() {
-        if (getPlayer().equalsIgnoreCase("x")) {
-            currentPlayer.setNaam("o");
-        } else currentPlayer.setNaam("x");
+        if (getPlayer() == 'X') {
+            currentPlayer.setNaam('O');
+        } else currentPlayer.setNaam('X');
     }
 
     @Override
--- src/Player.java
+++ src/Player.java
@@ -2,17 +2,17 @@
  * Created by Nicolas on 22-9-2015.
  */
 public class Player {
-    private String naam;
+    private char naam;
 
-    public Player(String naam) {
+    public Player(char naam) {
         this.naam = naam;
     }
 
-    public String getNaam() {
+    public char getNaam() {
         return naam;
     }
 
-    public void setNaam(String naam) {
+    public void setNaam(char naam) {
         this.naam = naam;
     }
 }
